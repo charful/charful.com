2ebe1b472c697a8d5957e581898c6513580e3690
Eduardo Esclapes <eduardo.esclapes@gmail.com>
2020-02-22 16:59:18 +0100

    typos

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Lifestyle for a carbon negative world." />
-    <h1>Lifestyle for a carbon negative world.</h1>
-    <p>We all wish to fght climate change.</p>
+    <h1>Lifestyle for a crbon negative world.</h1>
+    <p>We all wish to fight climate change.</p>
     <p>Be <em>charful</em> and let's do that together, everyday.</p>
     {/* <Link to="/blog/">Get onboard</Link> */}
   </Layout>
